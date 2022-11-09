e73f0060f86d8cbc6ccc606041fbd2c1d1418f67
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2022-11-09 19:22:36 +0100

    Add GUI

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.gordon.kanbanboard {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.base;
+
+    opens com.gordon.kanbanboard to javafx.fxml;
+    exports com.gordon.kanbanboard;
+}
